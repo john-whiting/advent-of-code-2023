ba5d6181846cbff3bfabb2b52d2f97b55324c875
John Whiting <thejwhiting@gmail.com>
2023-12-01 18:55:40 -0500

    feat: setup initial rust workspace

---
+++ target/debug/day-01.d
@@ -0,0 +1,1 @@
+/home/john/data/personal/lessons/advent-of-code-2023/target/debug/day-01: /home/john/data/personal/lessons/advent-of-code-2023/day-01/src/main.rs
---
+++ target/debug/deps/day_01-3959eb41f0d20b4c.d
@@ -0,0 +1,5 @@
+/home/john/data/personal/lessons/advent-of-code-2023/target/debug/deps/day_01-3959eb41f0d20b4c: day-01/src/main.rs
+
+/home/john/data/personal/lessons/advent-of-code-2023/target/debug/deps/day_01-3959eb41f0d20b4c.d: day-01/src/main.rs
+
+day-01/src/main.rs:
