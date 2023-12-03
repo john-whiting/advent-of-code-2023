797bd0b9f65ea35b443cd2c776fb52d9b88bd8fb
John Whiting <thejwhiting@gmail.com>
2023-12-03 17:19:00 -0500

    refactor(day-03): cleaned up "current_neighbor" function

--- day-03/src/neighborhood.rs
+++ day-03/src/neighborhood.rs
@@ -9,151 +9,72 @@ use crate::cell::Cell;
 #[derive(Clone, PartialEq, Debug)]
 pub struct Neighborhood<'a> {
     cell: &'a Cell<'a>,
-    left_idx: Option<usize>,
-    right_idx: Option<usize>,
-    up_idx: Option<usize>,
-    down_idx: Option<usize>,
-    count: u8,
+    neighbor_indices: [(Option<usize>, Option<usize>); 8],
+    count: usize,
 }
 
 impl<'a> Neighborhood<'a> {
     pub fn new(cell: &'a Cell) -> Self {
-        let max_row = cell.grid.rows.len() - 1;
-        let max_col = cell.grid.rows[0].len() - 1;
         Self {
             cell,
-            left_idx: if cell.col_idx == 0 {
-                None
-            } else {
-                Some(cell.col_idx - 1)
-            },
-            right_idx: if cell.col_idx == max_col {
-                None
-            } else {
-                Some(cell.col_idx + 1)
-            },
-            up_idx: if cell.row_idx == 0 {
-                None
-            } else {
-                Some(cell.row_idx - 1)
-            },
-            down_idx: if cell.row_idx == max_row {
-                None
-            } else {
-                Some(cell.row_idx + 1)
-            },
+            neighbor_indices: Neighborhood::make_neighbor_indices(cell),
             count: 0,
         }
     }
 
+    fn make_neighbor_indices(cell: &'a Cell) -> [(Option<usize>, Option<usize>); 8] {
+        let max_row = cell.grid.rows.len() - 1;
+        let max_col = cell.grid.rows[0].len() - 1;
+        let left_idx = if cell.col_idx == 0 {
+            None
+        } else {
+            Some(cell.col_idx - 1)
+        };
+        let right_idx = if cell.col_idx == max_col {
+            None
+        } else {
+            Some(cell.col_idx + 1)
+        };
+        let up_idx = if cell.row_idx == 0 {
+            None
+        } else {
+            Some(cell.row_idx - 1)
+        };
+        let down_idx = if cell.row_idx == max_row {
+            None
+        } else {
+            Some(cell.row_idx + 1)
+        };
+
+        let cur_row = Some(cell.row_idx);
+        let cur_col = Some(cell.col_idx);
+
+        [
+            (up_idx, left_idx),
+            (up_idx, cur_col),
+            (up_idx, right_idx),
+            (cur_row, left_idx),
+            (cur_row, right_idx),
+            (down_idx, left_idx),
+            (down_idx, cur_col),
+            (down_idx, right_idx),
+        ]
+    }
+
     fn current_neighbor(&self) -> Option<Cell<'a>> {
         let rows = &self.cell.grid.rows;
-        match self.count {
-            0 => {
-                if self.left_idx.is_some() && self.up_idx.is_some() {
-                    let row_idx = self.up_idx.unwrap();
-                    let col_idx = self.left_idx.unwrap();
-                    let value = rows[row_idx][col_idx];
-                    return Some(Cell {
-                        grid: self.cell.grid,
-                        value,
-                        row_idx,
-                        col_idx,
-                    });
-                }
-            }
-            1 => {
-                if let Some(up) = self.up_idx {
-                    let row_idx = up;
-                    let col_idx = self.cell.col_idx;
-                    let value = rows[row_idx][col_idx];
-                    return Some(Cell {
-                        grid: self.cell.grid,
-                        value,
-                        row_idx,
-                        col_idx,
-                    });
-                }
-            }
-            2 => {
-                if self.right_idx.is_some() && self.up_idx.is_some() {
-                    let row_idx = self.up_idx.unwrap();
-                    let col_idx = self.right_idx.unwrap();
-                    let value = rows[row_idx][col_idx];
-                    return Some(Cell {
-                        grid: self.cell.grid,
-                        value,
-                        row_idx,
-                        col_idx,
-                    });
-                }
-            }
-            3 => {
-                if let Some(left) = self.left_idx {
-                    let row_idx = self.cell.row_idx;
-                    let col_idx = left;
-                    let value = rows[row_idx][col_idx];
-                    return Some(Cell {
-                        grid: self.cell.grid,
-                        value,
-                        row_idx,
-                        col_idx,
-                    });
-                }
-            }
-            4 => {
-                if let Some(right) = self.right_idx {
-                    let row_idx = self.cell.row_idx;
-                    let col_idx = right;
-                    let value = rows[row_idx][col_idx];
-                    return Some(Cell {
-                        grid: self.cell.grid,
-                        value,
-                        row_idx,
-                        col_idx,
-                    });
-                }
-            }
-            5 => {
-                if self.left_idx.is_some() && self.down_idx.is_some() {
-                    let row_idx = self.down_idx.unwrap();
-                    let col_idx = self.left_idx.unwrap();
-                    let value = rows[row_idx][col_idx];
-                    return Some(Cell {
-                        grid: self.cell.grid,
-                        value,
-                        row_idx,
-                        col_idx,
-                    });
-                }
-            }
-            6 => {
-                if let Some(down) = self.down_idx {
-                    let row_idx = down;
-                    let col_idx = self.cell.col_idx;
-                    let value = rows[row_idx][col_idx];
-                    return Some(Cell {
-                        grid: self.cell.grid,
-                        value,
-                        row_idx,
-                        col_idx,
-                    });
-                }
-            }
-            7 => {
-                if self.right_idx.is_some() && self.down_idx.is_some() {
-                    let row_idx = self.down_idx.unwrap();
-                    let col_idx = self.right_idx.unwrap();
-                    let value = rows[row_idx][col_idx];
-                    return Some(Cell {
-                        grid: self.cell.grid,
-                        value,
-                        row_idx,
-                        col_idx,
-                    });
-                }
-            }
-            _ => (),
+        let (row_idx, col_idx) = self.neighbor_indices.get(self.count)?;
+
+        if row_idx.is_some() && col_idx.is_some() {
+            let row_idx = row_idx.unwrap();
+            let col_idx = col_idx.unwrap();
+            let value = rows[row_idx][col_idx];
+            return Some(Cell {
+                grid: self.cell.grid,
+                value,
+                row_idx,
+                col_idx,
+            });
         }
 
         None
